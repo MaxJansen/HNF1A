03cb60fd2b4b1b7b4559edb32cb4d1da30629e56
Max Jansen <maxlouis@well.ox.ac.uk>
2019-07-11 18:13:50 +0100

    all adapted to trim

--- 2.batches_trim_fastq.sh
+++ 2.batches_trim_fastq.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 origins="/well/mccarthy/production/rna-seq/data/StemBANCC/HNF1A_SS2_scRNA_remap"
 #jobs
-./2.start_large_table.sh "${origins}/Batch1/180410_K00150_0313_BHTV35BBXX/trim_fastq" \
+./start_large_table.sh "${origins}/Batch1/180410_K00150_0313_BHTV35BBXX/trim_fastq" \
 /well/mccarthy/users/maxlouis/oxford2/HNF1A_project/data/Batch1_trim
 
-./2.start_large_table.sh "${origins}/Batch2/180524_K00198_0314_BHV55CBBXX/trim_fastq" \
+./start_large_table.sh "${origins}/Batch2/180524_K00198_0314_BHV55CBBXX/trim_fastq" \
 /well/mccarthy/users/maxlouis/oxford2/HNF1A_project/data/Batch2_trim
 
-./2.start_large_table.sh "${origins}/Batch2/181205_K00198_0374_BH2NJKBBXY/trim_fastq" \
+./start_large_table.sh "${origins}/Batch2/181205_K00198_0374_BH2NJKBBXY/trim_fastq" \
  /well/mccarthy/users/maxlouis/oxford2/HNF1A_project/data/Batch2second_trim
 
-./2.start_large_table.sh "${origins}/Batch3/190322_K00181_0125_AH25MHBBXY/trim_fastq" \
+./start_large_table.sh "${origins}/Batch3/190322_K00181_0125_AH25MHBBXY/trim_fastq" \
  /well/mccarthy/users/maxlouis/oxford2/HNF1A_project/data/Batch3_trim
 
-./2.start_large_table.sh "${origins}/Batch4/190328_K00181_0127_BH25MJBBXY/trim_fastq" \
+./start_large_table.sh "${origins}/Batch4/190328_K00181_0127_BH25MJBBXY/trim_fastq" \
  /well/mccarthy/users/maxlouis/oxford2/HNF1A_project/data/Batch4_trim
